5f794e01b0fe18247c29aff6e4a8544e4966cf5b
BoD <BoD@JRAF.org>
2024-10-28 11:38:00 +0100

    Try getDeclarationsInSourceOrder per https://github.com/google/ksp/issues/2135#issuecomment-2438964740

--- processor/src/main/kotlin/org/example/processor/MySymbolProcessor.kt
+++ processor/src/main/kotlin/org/example/processor/MySymbolProcessor.kt
@@ -1,5 +1,6 @@
 package org.example.processor
 
+import com.google.devtools.ksp.KspExperimental
 import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessor
@@ -7,11 +8,15 @@ import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSFunctionDeclaration
 
 class MySymbolProcessor(private val logger: KSPLogger) : SymbolProcessor {
   override fun process(resolver: Resolver): List<KSAnnotated> {
     val classDeclarations = resolver.getSymbolsWithAnnotation("org.example.app.MyAnnotation").filterIsInstance<KSClassDeclaration>()
-    logger.warn("all functions:" + classDeclarations.first().getAllFunctions().toList().map { it.simpleName.asString() })
+    //val functions = classDeclarations.first().getAllFunctions().toList()
+    val functions = @OptIn(KspExperimental::class) resolver.getDeclarationsInSourceOrder(classDeclarations.first())
+      .filterIsInstance<KSFunctionDeclaration>().toList()
+    logger.warn("all functions:" + functions.map { it.simpleName.asString() })
     return emptyList()
   }
 }
